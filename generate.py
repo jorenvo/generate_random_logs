3c5af9a10c56cb5105c006c1459170ff8568f682
Joren Van Onder <joren.vanonder@gmail.com>
2019-04-01 21:17:57 -0700

    Add configurable API abuser

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import random
+import sys
 from collections import namedtuple
 from datetime import datetime, timedelta
 from scipy.stats import gamma
@@ -30,13 +31,14 @@ def get_endpoint():
     )
 
 
-def generate_lines(n):
+def generate_lines(n, abuse_ratio=None):
     fmt = "[{timestamp}] {ip} {endpoint} {time:.2f}"
     max_seconds = 100
     log_length = timedelta(days=30)
     end = datetime.now()
     begin = end - log_length
     output = []
+    bad_ip = random_ip()
 
     timestamps = [
         datetime.fromtimestamp(random.uniform(end.timestamp(), begin.timestamp()))
@@ -48,7 +50,9 @@ def generate_lines(n):
         output.append(
             fmt.format(
                 timestamp=timestamp,
-                ip=random_ip(),
+                ip=random.choice((bad_ip,) + (random_ip(),) * (abuse_ratio - 1))
+                if abuse_ratio
+                else random_ip(),
                 endpoint=endpoint.url,
                 time=gamma.rvs(endpoint.gamma_shape, scale=max_seconds / 10),
             )
@@ -59,7 +63,16 @@ def generate_lines(n):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--abuse-ratio",
+        help="How many times on average an abuser will do a request per [abuse ratio] requests",
+        type=int,
+    )
     parser.add_argument("lines", help="How many lines to generate", type=int)
     args = parser.parse_args()
 
-    print(generate_lines(args.lines))
+    if args.abuse_ratio < 1:
+        print("Abuse ratio must be >= 1.")
+        sys.exit(1)
+
+    print(generate_lines(args.lines, args.abuse_ratio))
